f3103f1e9c7401bc3f1a05d07869702043d7245b
Patryk Lekki <patryk.lekki@awesome-ind.com>
2022-03-20 17:03:25 +0100

    adding new completed tasks

--- src/test/zad6.java
+++ src/test/zad6.java
@@ -1,9 +1,41 @@
 package test;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class zad6 {
 
-    public static void main(String[] args) {
-        System.out.println("hello world");
+    static void splitText() throws IOException {
+
+        BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Patryk\\Desktop\\tak.txt"));
+        try {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+            }
+            String everything = sb.toString();
+            String[] splitText = everything.split(" ");
+
+
+            for(int i = 0; i < splitText.length; i++) {
+                System.out.println(i +1  + " " + splitText[i]);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            br.close();
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        splitText();
     }
 
 }
--- src/test/zad7.java
+++ src/test/zad7.java
@@ -1,4 +1,21 @@
 package test;
 
+import org.codehaus.jackson.JsonProcessingException;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import java.io.IOException;
+
+
 public class zad7 {
+
+    public static void main(String[] arg) throws JsonProcessingException, IOException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        User userObject = new User("John", 21);
+        String userJson = objectMapper.writeValueAsString(userObject);
+
+        System.out.println(userJson);
+    }
+
 }
