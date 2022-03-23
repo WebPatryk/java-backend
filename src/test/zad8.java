44a48102ce2e6989bdf69bf67c8d2516273dd4b5
Patryk Lekki <patryk.lekki@awesome-ind.com>
2022-03-23 20:24:04 +0100

    adding new tasks

--- src/test/zad8.java
+++ src/test/zad8.java
@@ -1,4 +1,35 @@
 package test;
 
+import org.codehaus.jackson.map.ObjectMapper;
+
+import java.io.IOException;
+
 public class zad8 {
+    public static class User {
+
+        private String name;
+        private int age;
+
+        public User(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public int getAge() {
+            return this.age;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        User userObject = new User("John",21);
+        String userJson = objectMapper.writeValueAsString(userObject);
+
+        System.out.println(userJson); // {"name":"John","age":21}
+    }
 }
