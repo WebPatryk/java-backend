8565bebaee1fc06360a78a8b49ce64dda229ba58
Patryk Lekki <patryk.lekki@awesome-ind.com>
2022-06-17 15:03:08 +0200

    Controller to REST API

--- projekt/backend/demo/src/main/java/repository/CarPartsRepository.java
+++ projekt/backend/demo/src/main/java/repository/CarPartsRepository.java
@@ -2,15 +2,14 @@ package repository;
 
 import com.example.demo.model.CarParts;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
 
 public interface CarPartsRepository extends JpaRepository<CarParts, Long> {
 
     void deleteCarPartsById(Long id);
 
-    Optional<CarParts> findById(Long aLong);
 
    Optional<CarParts> findAllCarPartsById(Long id);
 
--- projekt/backend/demo/src/main/java/service/CarPartsService.java
+++ projekt/backend/demo/src/main/java/service/CarPartsService.java
@@ -3,23 +3,26 @@ package service;
 import com.example.demo.model.CarParts;
 import exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import repository.CarPartsRepository;
 
-import java.nio.file.attribute.UserPrincipalNotFoundException;
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.UUID;
 
-
 @Service
+@Transactional
 public class CarPartsService {
-    private  final CarPartsRepository carPartsRepository;
+    private final CarPartsRepository carPartsRepository;
 
 
     @Autowired
     public CarPartsService(CarPartsRepository carPartsRepository) {
         this.carPartsRepository = carPartsRepository;
     }
+
 
     public CarParts addCarParts(CarParts carParts){
         carParts.setCode(UUID.randomUUID().toString());
@@ -31,8 +34,8 @@ public class CarPartsService {
     public CarParts updateCarParts(CarParts carParts){
         return  carPartsRepository.save(carParts);
     }
-    public void findAllCarPartsById(Long id){
-        carPartsRepository.findAllCarPartsById(id).orElseThrow(()-> new UserNotFoundException("Car parts by id " + id + "was not found"));
+    public CarParts findAllCarPartsById(Long id){
+        return carPartsRepository.findAllCarPartsById(id).orElseThrow(()-> new UserNotFoundException("Car parts by id " + id + "was not found"));
     }
 
     public void deleteCarParts(Long id){
